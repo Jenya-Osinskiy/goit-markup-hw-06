5829510d0d0aa5bcc0a44ae7fbc27bb717f12243
Jenya Osinskiy <157902732+Jenya-Osinskiy@users.noreply.github.com>
2024-03-30 20:07:36 +0200

    update

---
+++ modal.js
@@ -0,0 +1,25 @@
+let buttonsectionone = document.querySelector('.button-section-one');
+let modaloverlay = document.querySelector('.modal-overlay');
+let modalclosebut = document.querySelector('.modal-close-but');
+
+buttonsectionone.addEventListener('click', () => {
+    modaloverlay.classList.add('is-open');
+})
+
+modalclosebut.addEventListener('click', (e) => {
+    e.preventDefault();
+    modaloverlay.classList.remove('is-open');
+})
+
+let navbtnopen = document.querySelector('.nav-btn-open');
+let mobnav = document.querySelector('.mob-nav');
+let mobclosebut = document.querySelector('.mob-close-but');
+
+navbtnopen.addEventListener('click', () => {
+    mobnav.classList.add('is-open');
+})
+
+mobclosebut.addEventListener('click', (e) => {
+    e.preventDefault();
+    mobnav.classList.remove('is-open');
+})
